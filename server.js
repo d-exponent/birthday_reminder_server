df74f15df8b8f5c7fb00334de2826457e53e56cb
Desmond Odion <106418626+d-exponent@users.noreply.github.com>
2023-08-01 18:36:43 +0100

    redesigned mongoose connection strategy

--- server.js
+++ server.js
@@ -1,29 +1,38 @@
 const mongoose = require('mongoose')
+const connectDatabase = require('./src/utils/db-connect')
 const env = require('./src/settings/env')
+const reminderJob = require('./src/cron-jobs/birthday_reminder')
 const port = env.port
 const app = require('./src/app')()
 
-const server = app.listen(port, () =>
-  console.log(`🤖[SERVER] is running on port`, port)
-)
+// const server = app.listen(port, () =>
+//   console.log(`🤖[SERVER] is running on port`, port)
+// )
+
+connectDatabase()
+  .then(res => {
+    console.log(res)
+    reminderJob.start()
+    app.listen(port, () => console.log(`🤖[SERVER] is running on port`, port))
+  })
+  .catch(error => console.error(error))
 
 module.exports = app
 
 // START CRON JOB
-require('./src/cron-jobs/birthday_reminder').start()
 
-const shutDownGracefully = (code = 1) => {
-  return (err, reason) => {
-    console.error('🛑🛑 SERVER ERROR =>  ', err, '🛑REASON => ', reason)
-    server.close()
-    mongoose.connection.close()
-    console.error('All connections closed successfully')
-    process.exit(code)
-  }
-}
+// const shutDownGracefully = (code = 1) => {
+//   return (err, reason) => {
+//     console.error('🛑🛑 SERVER ERROR =>  ', err, '🛑REASON => ', reason)
+//     server.close()
+//     mongoose.connection.close()
+//     console.error('All connections closed successfully')
+//     process.exit(code)
+//   }
+// }
 
-// Handle shutdown events
-process
-  .on('SIGTERM', shutDownGracefully(0))
-  .on('uncaughtException', shutDownGracefully())
-  .on('unhandledRejection', shutDownGracefully())
+// // Handle shutdown events
+// process
+//   .on('SIGTERM', shutDownGracefully(0))
+//   .on('uncaughtException', shutDownGracefully())
+//   .on('unhandledRejection', shutDownGracefully())
--- src/app.js
+++ src/app.js
@@ -5,13 +5,11 @@ const mongoSanitize = require('express-mongo-sanitize')
 const rateLimit = require('express-rate-limit')
 const morgan = require('morgan')
 
+const app = express()
 const apiV1Router = require('./routes/api-v1')
 const env = require('./settings/env')
-const errorController = require('./controllers/errors')
 const appController = require('./controllers/app')
-const catchAsync = require('./utils/catch-async')
-const connectDatabase = require('./utils/db-connect')
-const app = express()
+const errorController = require('./controllers/errors')
 
 // MIDDLEWARE CONFIGS
 const corsConfig = {
@@ -27,15 +25,10 @@ const rateLimitConfig = {
   legacyHeaders: false
 }
 
-// To test successfull deployment to vercel. WIll be deleted
+// To test successfull deployment to vercel. Might be deleted
 const homeController = (_, res) => {
   res.send('App is up and running')
 }
-
-const dbConnect = catchAsync(async (_, __, next) => {
-  await connectDatabase()
-  next()
-})
 
 module.exports = () => {
   app.use(cors(corsConfig))
@@ -49,7 +42,6 @@ module.exports = () => {
   app.use(appController.mountRefreshTokenManager)
 
   app.get('/', homeController)
-  app.use(dbConnect)
   app.use('/api/v1', apiV1Router)
   app.use('*', errorController.wildRoutesHandler)
   app.use(errorController.globalErrorHandler)
